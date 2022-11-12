9beacc800c63146ea5a182e96a83e0db2978b292
CharmingDuck <88068148+CharmingDuck@users.noreply.github.com>
2022-11-12 11:34:59 -0600

    Add files via upload

---
+++ Interleaved IO.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int x;
+   
+   cout << "Enter x: ";
+   cin >> x;
+   
+    // Student mistakenly is echo'ing the input to output to match example
+   cout << "x doubled is: " << 2 * x << endl;
+
+   return 0;
+}

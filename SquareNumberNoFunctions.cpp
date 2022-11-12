03f3e9f0762d94bb69cd753bf15adf4f3cf34085
CharmingDuck <88068148+CharmingDuck@users.noreply.github.com>
2022-11-12 11:23:15 -0600

    Add files via upload

---
+++ SquareNumberNoFunctions.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+
+int main() 
+{
+   int userNum;
+   int userNumSquared;
+   
+   cout << "Enter a number: ";
+   cin >> userNum;
+   
+   userNumSquared = userNum * userNum;   // Bug here; fix it when instructed
+   
+   cout << userNumSquared << endl;   // Output formatting issue here; fix it when instructed
+   
+   return 0;
+}
